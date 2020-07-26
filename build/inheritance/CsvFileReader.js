bd41ebcae6be4ebab4e0313f1d47ff4ad3cd2a98
Bilguun <bbg.1015@gmail.com>
2020-07-26 13:45:54 -0500

    refactored index.ts to instantiate 2 different interface

---
+++ build/inheritance/CsvFileReader.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CsvFileReader = void 0;
+var fs_1 = __importDefault(require("fs"));
+// Using Generic and Abstact class to achieve reusability
+var CsvFileReader = /** @class */ (function () {
+    function CsvFileReader(filename) {
+        this.filename = filename;
+        this.data = [];
+    }
+    CsvFileReader.prototype.read = function () {
+        this.data = fs_1.default
+            .readFileSync(this.filename, {
+            // return string to us with encoding
+            // if encoding wasn't added, it would bring us raw data called buffer
+            encoding: 'utf-8',
+        })
+            .split('\n')
+            .map(function (row) {
+            return row.split(',');
+        })
+            .map(this.mapRow);
+    };
+    return CsvFileReader;
+}());
+exports.CsvFileReader = CsvFileReader;
