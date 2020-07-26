f01eb19be9cb5066c7c8e993469c0138e268b837
Bilguun <bbg.1015@gmail.com>
2020-07-25 22:15:46 -0500

    using tuple instead of array of arrays

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -29,6 +29,8 @@ var CsvFileReader = /** @class */ (function () {
                 row[2],
                 parseInt(row[3]),
                 parseInt(row[4]),
+                row[5],
+                row[6],
             ];
         });
     };
